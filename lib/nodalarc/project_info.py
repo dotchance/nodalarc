d1ccfe6b78b3efcdf37c7cc17db61d0c18101527
dotchance <dotchance@users.noreply.github.com>
2026-05-19 22:03:27 +0000

    Add project attribution metadata

---
+++ lib/nodalarc/project_info.py
@@ -0,0 +1,23 @@
+# Copyright 2024-2026 .chance (dotchance)
+# Licensed under the NodalArc Source Available License 1.0. See LICENSE file.
+"""Project attribution metadata shared by API and tooling surfaces."""
+
+PROJECT_NAME = "NodalArc"
+PROJECT_VERSION = "0.1.0"
+PROJECT_AUTHOR = ".chance (dotchance)"
+PROJECT_COPYRIGHT = "Copyright 2024-2026 .chance (dotchance)"
+PROJECT_SOURCE_URL = "https://github.com/dotchance/nodalarc"
+PROJECT_URL = "https://nodal.asmolab.net"
+PROJECT_LICENSE = "NodalArc Source Available License 1.0"
+PROJECT_NOTICE = "See NOTICE and THIRD_PARTY_NOTICES.md."
+
+PROJECT_ATTRIBUTION = {
+    "name": PROJECT_NAME,
+    "version": PROJECT_VERSION,
+    "author": PROJECT_AUTHOR,
+    "copyright": PROJECT_COPYRIGHT,
+    "source": PROJECT_SOURCE_URL,
+    "url": PROJECT_URL,
+    "license": PROJECT_LICENSE,
+    "notice": PROJECT_NOTICE,
+}
